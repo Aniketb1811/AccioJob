813835b0bec37209469752ea255047565bc37121
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-01-01 22:48:46 +0530

    Create Fahrenheit to Celsius.java

---
+++ Easy/Fahrenheit to Celsius.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+                int f = sc.nextInt();
+                int c = (f-32)*5/9;
+                System.out.println(c);
+	}
+}
