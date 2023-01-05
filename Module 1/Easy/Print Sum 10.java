a4f346bf95910580fb1518f1ba68d224fef034f3
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-01-05 10:48:49 +0530

    Create Print Sum 10.java

---
+++ Module 1/Easy/Print Sum 10.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+                int sum = 0;
+                for(int i = 1; i <=10; i++)
+                        sum += i;
+
+                System.out.println(sum);
+	}
+}
