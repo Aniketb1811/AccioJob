a0f2e77fa5f71065cf29d5a8a1025d8a45dd0081
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-01-09 21:01:43 +0530

    Create Switch Case Problem 1.java

---
+++ Module 1/Easy/Switch Case Problem 1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)  throws java.lang.Exception{
+         //Write code here and print output
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		switch (n){
+		case 55 : 
+				System.out.print("Study is important");
+				break;
+		default:
+				System.out.print("No value");
+				
+		}
+    }
+}
