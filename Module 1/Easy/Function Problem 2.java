d732cf97c65c3f5dc77b2261835fa95c7f9d6800
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-01-09 20:58:53 +0530

    Create Function Problem 2.java

---
+++ Module 1/Easy/Function Problem 2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        fun(m,n);
+}
+    public static void fun(int a, int b){
+        System.out.println(a+b);
+    }
+}
