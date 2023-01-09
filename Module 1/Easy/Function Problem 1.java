2c12fee20335f16c1e5c4460e812d7a24447860e
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-01-09 21:01:18 +0530

    Create Function Problem 1.java

---
+++ Module 1/Easy/Function Problem 1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+		fun();
+    }
+
+	public static void fun(){
+			String str = "I am another function";
+			System.out.print(str);
+		}
+
+}
