4d0824b98994af81b1f717d7eda46147e776f8f9
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-01-20 14:19:59 +0530

    Create Variable Practice Question 8.java

---
+++ Gym/Module 1/Easy/Variable Practice Question 8.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+		int a = 8;
+		for(int i = 1; i <= 10; i++){
+			System.out.println(a + " x " + i + " = " + a*i); 
+		}
+    }
+}
