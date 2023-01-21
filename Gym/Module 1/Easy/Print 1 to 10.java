e93854f677eb5efb02ac65f1defde9efc659cec4
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-01-21 23:25:57 +0530

    Create Print 1 to 10.java

---
+++ Gym/Module 1/Easy/Print 1 to 10.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    static void print1to10() {
+        //Write your code here
+		for(int i=1; i<=10; i++){
+			System.out.println(i);
+		}
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Solution.print1to10();
+    }
+}
