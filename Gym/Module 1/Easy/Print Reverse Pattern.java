2e2d118b98e312abd6ee9f90bae821bfb318c397
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-01-22 18:58:45 +0530

    Create Print Reverse Pattern.java

---
+++ Gym/Module 1/Easy/Print Reverse Pattern.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for(int i = 0; i < n; i++){
+			for(int k = 0; k < i; k++){
+				System.out.print(" ");
+			}
+			for(int j = n - i; j > 0; j--){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+    }
+}
