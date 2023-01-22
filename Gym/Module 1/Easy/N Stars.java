62edc4f77fc6247d8c2b14423de31c87f6172c95
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-01-22 18:46:59 +0530

    Create N Stars.java

---
+++ Gym/Module 1/Easy/N Stars.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for(int i=1; i<=n; i++){
+			System.out.print("* ");
+		}
+		System.out.println();
+		for(int i=1; i<=n; i++){
+			System.out.println("*");
+		}
+    }
+}
