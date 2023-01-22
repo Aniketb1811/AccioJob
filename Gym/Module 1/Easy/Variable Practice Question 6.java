aef5453a327c740f908a627087b566ec2b3993a6
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-01-22 18:50:33 +0530

    Create Variable Practice Question 6.java

---
+++ Gym/Module 1/Easy/Variable Practice Question 6.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+		int a = 1021;
+		int b = 20;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+    }
+}
