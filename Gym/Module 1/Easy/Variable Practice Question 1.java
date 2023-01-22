8e646fd78dafb999194f0a4ecca094dd0e386257
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-01-22 18:42:41 +0530

    Create Variable Practice Question 1.java

---
+++ Gym/Module 1/Easy/Variable Practice Question 1.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+		int a = 10;
+		System.out.print("My num is " + a);
+    }
+}
