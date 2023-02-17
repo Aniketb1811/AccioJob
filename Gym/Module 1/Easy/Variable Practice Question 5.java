6d4f26b177d9ea5c2f99d761983d7d1823223a66
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-02-17 09:11:03 +0530

    Create Variable Practice Question 5.java

---
+++ Gym/Module 1/Easy/Variable Practice Question 5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+		float v1 = 10.5f;
+		float v2 = 12.5f;
+		float v3 = 12.15f;
+		float avg = (v1+v2+v3)/3f;
+
+		System.out.print((int)avg);
+	}
+}
